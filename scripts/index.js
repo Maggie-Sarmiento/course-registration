7e93c2ccbb133972f9182e03952b97a0e6467f8d
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-17 21:30:52 -0500

    Clase 3.4 Mostrando más cursos en el DOM

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,46 @@
 /**
- * Clase 3.3 IMPRIMIENDO CONTENIDO EN EL DOM
+ * Clase 3.4 MOSTRANDO MÁS CURSOS EN EL DOM
  */
 
 import Course from "./classes/Course.js"
 
+// Instanciando objetos de la clase Course
 const html = new Course("HTML dede cero", "https://cdn.pixabay.com/photo/2014/07/08/09/58/html5-386614_960_720.jpg", 10)
 const css = new Course("CSS dede cero", "https://media.istockphoto.com/vectors/concept-stamped-word-art-illustration-vector-id906564624?s=612x612", 8)
 
-console.log(html)
-console.log(css)
 
-const element = document.getElementById("course")
-const element2 = document.getElementById("course2")
+const element = document.getElementById("courses")
 
-element.innerHTML = `
-<h3>${html.getName()}</h3>
-<img src="${html.getPoster()}" />
-<span> Cantidad de cursos: ${html.getSubjects()} </span>
+// Imprime curso en el Dom
+// Recibe un objeto tipo Course
+function showCourse(curse) {
+    const hijo = document.createElement("div")
+hijo.innerHTML = `
+<h3>${curse.getName()}</h3>
+<img src="${curse.getPoster()}" />
+<span> Cantidad de cursos: ${curse.getSubjects()} </span>
 `
+element.appendChild (hijo)
+}
 
-element2.innerHTML = `
-<h3>${css.getName()}</h3>
-<img src="${css.getPoster()}" />
-<span> Cantidad de cursos: ${css.getSubjects()} </span>
-`
+// Llamadas a la función mostrar curso
+showCourse(html)
+showCourse(css)
 
-/* La estructura que has creado en el archivo .js de la clase que vas a utilizar te servirá para poder insertar contenido dinámico de una clase, en tu HTML haciendo un inner.html
 
-Deberás crear la estructura en HTML donde quieras que este contenido sea insertado.
+/* El parámetro inner.HTML solo podrá mostrar 1 objeto en el DOM, para poder mostrar varios objetos se utilizará appendChild()
 
-¿Cómo insertar este contenido dinámico?
-Crearás una constante que será el document.getElementById("nombreDeId") el nombre que le hayas colocado a tu constante lo implementarás con el inner.html
+Con appendChild() ya no se creará el contenido a imprimir en el DOM dentro de las template strings, sino que, se creará un elemento como parámetro.
 
-¿Cómo imprimir el contenido de la clase en el DOM?
-Para imprimir en el DOM usarás el nombre de tu constante más la nomenclatura punto y el parámetro inner.html y luego le asignarás con template strings los valores que vayas a utilizar. Dentro de las template strings o backstick crearás el contenido a imprimir en el DOM y dentro de una variable traerás con el método get los atributos de la clase.
+Dentro de una constante la cual deberás asignarle el mismo nombre del elemento de appendChild() le asignarás a esta constante un document.createElement("div") el div se creará cuando el elemento sea llamado.
 
-EJ: `constante.innerHTML =
+Luego llamarás a tu elemento del appendChild(elemento) más él innerHTML y a esto le asignarás toda la información contenida en las template strings.
 
-<h3>${nombreDelObjeto.getNombreDelMetodo()}</h3> */
+En resumen, crearás un elemento donde entrarás el contenido, crearás un hijo que estará dentro de ese elemento y será él div. Al hijo se le agregará él inner.HTML y se introducirá con appendChild() el hijo.
+
+Para agregar nuevos cursos esto implicará repetir el contenido y como ya sabes, esto no es una buena práctica por lo que se procedería a crear una función. Todas las indicaciones anteriores deberás colocarlas dentro de una función.
+
+¿Cómo puedes hacer para que tu objeto sea dinámico?
+A tu función le deberás dar un parámetro para que este sea la constante de cada atributo del objeto y al llamar la función, le deberás colocar la constante donde está creada la clase.
+
+Como buena práctica siempre es recomendable documentar la funcionalidad de tu código. */
