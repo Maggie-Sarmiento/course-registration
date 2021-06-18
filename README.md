cd6e2ad05d6cb896c52713dd4a5a244ff68f61e8
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-17 19:46:08 -0500

    course-registration clase 3.1 Estructura del proyecto

---
+++ README.md
@@ -0,0 +1,4 @@
+# course-registration - app with javascript-POO
+
+Small app that allows to register courses and users for the same courses.
+The users can be teacher or students.
---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+/**
+ * Clase 3.1 ESTRUCTURA DEL PROYECTO
+ */
+
+/*  Una buena práctica de cada proyecto que se va a iniciar es, que primero se debe crear el archivo README.md. (md de markdown) En este archivo escribirás la documentación del proyecto.
+
+ Para agregar una librería externa en tu proyecto:
+ Agregar la etiqueta link
+ En la dirección colocar la url de la librería.
+ Ya todo quedaría listo para utilizar las guías de estilo de la librería en tu proyecto.
+ Los archivos llamados index, le indican al navegador que este es el archivo principal de una carpeta o sub-carpeta.
+ 
+ Como buena práctica es recomendable probar si tu script está funcionando correctamente en el navegador, esto lo podrás realizar con un simple alert("Hola EDteam") como también un console.log("EDteam"). */
+
+
+
+
+
+
+
+
+
+ 
