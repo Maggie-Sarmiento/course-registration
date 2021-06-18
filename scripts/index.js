1385d409058c77b256b64ce9b16a2fa6d6d352d2
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-17 22:51:07 -0500

    Clase 3.7 Creando objetos desde el DOM

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 /**
- * Clase 3.6 MARCADO DE FORMULARIO
+ * Clase 3.7 Creando objetos desde el DOM
  */
 
 import Course from "./classes/Course.js"
@@ -25,8 +25,25 @@ function showCourse(curse) {
     element.appendChild (hijo)
 }
 
-/* En el mundo real, también se utiliza la interfaz web para permitirle al usuario crear contenido desde un navegador. La forma correcta de realizar esta acción será creando un formulario y permitirle a este formulario crear objetos que se impriman en el DOM.
+const formulario = document.getElementById("formCursos")
+formulario.addEventListener("submit", event => {
+    event.preventDefault()
+    const target = event.target
+    const course = new Course(target.nameCourse.value, target.posterCourse.value, target.classCourse.value)
+    showCourse(course)
+})
 
-Dirígete a tu index.html y crea un formulario con ed-grid
 
-En la url de la web por defecto aparecerán todos los valores que se enviaron en el formulario hay que evitar que esto suceda y agarrar estos valores para hacer con ellos lo que se necesite. */
+/* Para poder procesar los valores del formulario y que aparezca en tu JavaScript harás lo siguiente.
+
+Crearás una variable y le asignarás un document.getElementById(" ") y le colocarás entre comillas el nombre de id del formulario que creaste en HTML. Ya creada la constante podrás comenzar a trabajar con lo que necesites.
+
+Al nombre de la constante le podrás dar un addEventListener() esto para esperar un evento, en este caso el evento submit, y que cuando suceda el evento submit ejecute una función. Aquí necesitarás indicarle que cuando se le dé click al evento submit del formulario suceda una acción. La primera acción será que el evento del formulario no se ejecute. e.preventDefault().
+
+Almacenar en una constante el evento e asignándole un .target e.target.
+
+Con el atributo name de los input en el html traerás la información de cada input y para acceder al valor del input le agregarás un .value.
+
+Una vez hayas accedido a todos los valores, crearás un curso dentro de una constante.
+
+EJ: const curso = new Curso(target.nombreCurso.value) y separado por comillas, los demás valores necesarios. A esta constante la podrás llamar con la función que la mostrará en el DOM. */
