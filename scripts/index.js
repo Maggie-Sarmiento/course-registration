3ef8d7250acd71b08cf1f0924fbd4c436a39c5fd
Maggie-Sarmiento <gsmaggie001@gmail.com>
2021-06-17 22:21:18 -0500

    Clase 3.6 Marcado de formulario

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,8 @@
 /**
- * Clase 3.5 APLICANDO CLASES DE EDux
+ * Clase 3.6 MARCADO DE FORMULARIO
  */
 
 import Course from "./classes/Course.js"
-
-// Instanciando objetos de la clase Course
-const html = new Course("HTML dede cero", "https://cdn.pixabay.com/photo/2014/07/08/09/58/html5-386614_960_720.jpg", 10)
-const css = new Course("CSS dede cero", "https://media.istockphoto.com/vectors/concept-stamped-word-art-illustration-vector-id906564624?s=612x612", 8)
-
 
 const element = document.getElementById("courses")
 
@@ -22,19 +17,16 @@ function showCourse(curse) {
         </div>
         <div class="card__data s-border s-radius-br s-radius-bl s-pxy-2">
             <h3 class="t5 s-mb-2 s-center">${curse.getName()}</h3>
-            <span class="small"># de clases : ${curse.getSubjects()}</span>
+            <div class="s-center">
+                <span class="small"># de clases : ${curse.getSubjects()}</span>
+            </div>
         </div>
     `
     element.appendChild (hijo)
 }
 
-// Llamadas a la función mostrar curso
-showCourse(html)
-showCourse(css)
+/* En el mundo real, también se utiliza la interfaz web para permitirle al usuario crear contenido desde un navegador. La forma correcta de realizar esta acción será creando un formulario y permitirle a este formulario crear objetos que se impriman en el DOM.
 
+Dirígete a tu index.html y crea un formulario con ed-grid
 
-/* En la web EDux encontrarás en la sección cards la estructura de como deberías crear un artículo para que se vea presentable, las clases que deberás utilizar son las mismas que están escritas en el blog, ya que este es una librería.
-
-Estas clases las deberás agregar en el div que has creado anteriormente en JS con el nombre que le hayas dado al elemento hijo. hijo.classList.add("clase")
-
-También dentro de tú hijo.innerHTML dentro de las template strings podrás seguir agregando la estructura de estilos para completar tus guías. */
+En la url de la web por defecto aparecerán todos los valores que se enviaron en el formulario hay que evitar que esto suceda y agarrar estos valores para hacer con ellos lo que se necesite. */
